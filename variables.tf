670246c4c29d33da594aff10a670910ca41c54d4
kyeongju <ainimo2@gmail.com>
2022-12-14 21:08:50 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -152,7 +152,7 @@ variable "access_logs" {
 
 variable "subnets" {
   description = "A list of subnets to associate with the load balancer. e.g. ['subnet-1a2b3c4d','subnet-1a2b3c4e','subnet-1a2b3c4f']"
-  type        = list
+  type        = list(string)
   default     = null
 }
 
