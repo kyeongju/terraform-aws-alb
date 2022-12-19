84427cfad65a82d70ed7e28b60e1a12b51bf1c7d
kyeongju <ainimo2@gmail.com>
2022-12-19 23:35:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   create_lb = var.create_lb && var.putin_khuylo
   create_lb_listener = var.create_lb_listener
   create_lb_tg = var.create_lb_tg
-  #https_listener_rules = var.https_listener_rules 
+  listener_rule = var.listener_rule 
 }
 
 resource "aws_lb" "this" {
@@ -422,7 +422,7 @@ resource "aws_lb_listener_rule" "https_listener_rule" {
 }
 
 resource "aws_lb_listener_rule" "http_tcp_listener_rule" {
-  count = local.create_lb ? length(var.http_tcp_listener_rules) : 0
+  count = local.create_lb || local.listener_rule ? length(var.http_tcp_listener_rules) : 0
 
   listener_arn = aws_lb_listener.frontend_http_tcp[lookup(var.http_tcp_listener_rules[count.index], "http_tcp_listener_index", count.index)].arn
   priority     = lookup(var.http_tcp_listener_rules[count.index], "priority", null)
