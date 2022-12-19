73d28e044ff6d89d71dfda0bf618ea2b78466fc2
coille02 <kb.coille@gmail.com>
2022-12-20 01:12:08 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -424,7 +424,7 @@ resource "aws_lb_listener_rule" "https_listener_rule" {
 resource "aws_lb_listener_rule" "http_tcp_listener_rule" {
   count = local.create_lb || local.create_listener_rule ? length(var.http_tcp_listener_rules) : 0
 
-  listener_arn = aws_lb_listener.frontend_http_tcp[lookup(var.http_tcp_listener_rules[count.index], "http_tcp_listener_index", count.index)].arn
+  listener_arn = local.create_lb ? aws_lb_listener.frontend_http_tcp[lookup(var.http_tcp_listener_rules[count.index], "http_tcp_listener_index", count.index)].arn : var.http_tcp_listener_arns
   priority     = lookup(var.http_tcp_listener_rules[count.index], "priority", null)
 
   # redirect actions
@@ -476,8 +476,8 @@ resource "aws_lb_listener_rule" "http_tcp_listener_rule" {
 
     content {
       type             = action.value["type"]
-      target_group_arn = aws_lb_target_group.main[lookup(action.value, "target_group_index", count.index)].id
-    }
+      target_group_arn = local.create_lb ? aws_lb_target_group.main[lookup(action.value, "target_group_index", count.index)].id : var.target_group_arn
+      }
   }
 
   # weighted forward actions
