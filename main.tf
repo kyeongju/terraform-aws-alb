db6e0664acdf101bf51ce98e3a4d94402034d4bb
coille02 <kb.coille@gmail.com>
2022-12-20 02:21:10 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -476,7 +476,7 @@ resource "aws_lb_listener_rule" "http_tcp_listener_rule" {
 
     content {
       type             = action.value["type"]
-      target_group_arn = local.create_lb ? aws_lb_target_group.main[lookup(action.value, "target_group_index", count.index)].id : var.http_tcp_listener_rules[count.index]["actions"].target_group_arn
+      target_group_arn = local.create_lb ? aws_lb_target_group.main[lookup(action.value, "target_group_index", count.index)].id : action.value.["target_group_arn"]
       }
   }
 
